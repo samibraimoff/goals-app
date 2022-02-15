ad23483b4b22612a8f0da294cc875ac0035e8582
Sandi Ibraimoff <sam.ibraimoff@gmail.com>
2022-02-15 02:24:00 -0500

    header and navigation, login and register pages added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Fragment>
+      <header className='header'>
+        <div className='logo'>
+          <Link to='/'>GoalSetter</Link>
+        </div>
+        <ul>
+          <li>
+            <Link to='/login'>
+              <FaSignInAlt /> Login
+            </Link>
+          </li>
+
+          <li>
+            <Link to='/register'>
+              <FaUser /> Register
+            </Link>
+          </li>
+        </ul>
+      </header>
+    </Fragment>
+  );
 };
 
 export default Header;
